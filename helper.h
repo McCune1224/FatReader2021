9c282b544a91c6c563536b9271a6b0d1d23275ef
Luke Frisbee <lukebfrisbee@gmail.com>
2021-02-22 15:00:53 -0800

    Added Base C Files

--- helper.h
+++ helper.h
@@ -2,11 +2,11 @@
 #define HELPER_HEADER
 #include <stdint.h>
 
-char* FileAttributes(uint8_t)
-char* AsHumanSize(int bytes)
-char* MediaType(uint8_t)
-char* PartitionTypeName(uint8_t)
-void HexDump(void*, int size)
-void FixShortFile(void*)
+char* FileAttributes(uint8_t);
+char* AsHumanSize(int bytes);
+char* MediaType(uint8_t);
+char* PartitionTypeName(uint8_t);
+void HexDump(void*, int size);
+void FixShortFile(void*);
 
 #endif
