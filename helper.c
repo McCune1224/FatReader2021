1508775890e039757467acf6a22689dd4b73c8f9
Kevin Alonso <kevinalonso@Kevins-MBP.lan>
2021-02-23 13:48:05 -0800

    added partition type function

--- helper.c
+++ helper.c
@@ -23,9 +23,37 @@ char* MediaType(uint8_t)
 
 // Kevin
 // Reads input parameter of 1 byte and responds with telling us the File System. For example, reading the byte will tell us if the file system is FAT16, FAT32, NTFS, etc.
-char* PartitionTypeName(uint8_t)
-{
-
+char* PartitionTypeName(uint8_t PartitionType)
+{  //binary to hex?
+   //printf("Read in the byte and decide PartitionType");
+   
+   
+   switch(PartitionType)
+   {
+       case 0x01:
+            return ("0x01 is FAT12");
+               
+       case 0x04:
+            return ("0x04 is FAT16");
+            
+       case 0x06:
+            return ("0x06 is FAT16B");
+        
+        case 0x07:
+            return("0x07 is NTFS");
+        
+        case 0x0B:
+            return("0x0B is FAT32 With CHS Addressing");
+        
+        case 0x0C:
+            return("0x0C is FAT32 With LBA");
+            
+        default:
+            return ("Partition Type is unknown");
+            
+   }
+   
+   
 }
 
 // Ali
