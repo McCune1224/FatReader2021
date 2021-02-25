8dada0f54037eed563feb53c0eb8bb2f77fced3a
Luke Frisbee <lukebfrisbee@gmail.com>
2021-02-24 16:33:34 -0800

    Merged Yunhu Code

--- test.c
+++ test.c
@@ -4,9 +4,19 @@
 #include <inttypes.h>
 #include "helper.h"
 
+uint8_t attributeNum = 235; // 11101011 eb
+
+uint8_t partitionNum = 1;
 
 int main(int argc, char* argv[])
 {
+    char* attributes = FileAttributes(attributeNum);
+    printf("%s\n", attributes);
+    free(attributes);
+
+	char* partitionType = PartitionTypeName(partitionNum);
+	printf("%s\n", partitionType);
+
 	uint64_t bytesBeforeConverted = 47281957121;
 	printf("%s\n", AsHumanSize(bytesBeforeConverted));
 	uint8_t byte = 0xE5;
