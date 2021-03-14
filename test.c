d20116e067643ff9e95a5dcf79b088f71ceac960
aamindehkordi <77307170+aamindehkordi@users.noreply.github.com>
2021-03-13 20:34:00 -0800

    Update test.c

--- test.c
+++ test.c
@@ -24,4 +24,33 @@ int main(int argc, char* argv[])
 
 	char* dumpTest = "\x00\x00Test...?..sdjfasjdfj\0asd;lasm;d\x00\x001,,0maks,";
 	hexDump(dumpTest, 32);
+
+
+{
+    char* filename = "dfr-16-fat.dd";
+ 
+    struct stat fs = {0};
+    if (stat(filename, &fs) != 0)
+    {
+        printf("Error: Could not read file statistics for '%s'\n", filename);
+        return 0;
+    } 
+ 
+    FILE* fin = fopen(filename, "rb");
+    if (fin == NULL)
+    {
+        printf("Error: Could not open binary file '%s'\n", filename);
+        return 0;
+    }
+
+    int offsetToFatTable = 0x810800; // taken from hex dump (unique to this image)
+    int count = 2;                   // taken from boot sector as seen in hex dump (pretty standard)
+    int fat_sectors = 254;           // taken from boot sector as seen in hex dump (unique to this image)
+    int sector_size = 512;           // taken from boot sector as seen in hex dump (pretty standard)
+ 
+    FAT_TABLE* fat = ReadFatTable(fin, offsetToFatTable, count, fat_sectors, sector_size);
+    printf("data: %08x\n", *(unsigned int*)fat);
+
+    return 1;
+
 }
