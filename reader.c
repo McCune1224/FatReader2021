d9641a6968f4daf3652556540f59fc375d526f0b
Kevin Alonso <kevinalonso@Kevins-MBP.lan>
2021-03-13 23:05:22 -0800

    Added MBR Struct and Read Function

--- reader.c
+++ reader.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "reader.h"
 
+
 int ReadDiskImage(char* filename)
 {
     return NULL;
@@ -8,7 +9,28 @@ int ReadDiskImage(char* filename)
 
 MBR* ReadMsterBootRecord(FILE* fp, long int offset)
 {
-    return NULL;
+    
+    int rc = fseek(fp, offset, SEEK_SET);
+    if (rc != 0) //if fseek equals 0, it is then successful. If it returns a nonzero, it has failed
+    {
+        printf("fseek failed, did not reach correct location\n");
+        return 0;
+    }
+    
+    char* buffer = (char*)malloc(sizeof(MBR));
+    if (buffer == NULL) //when buffer equals, null it will return a null byte which indicates lack of memory
+    {
+        printf("Error, not enough memory\n");
+        return NULL;
+    }
+    int count = fread(buffer, 1, sizeof(MBR), fp); 
+    if (count < sizeof(MBR)) //if the count is less than the size of MBR, it wont be able to read the contents of the file
+    {
+        printf("The contents of the file are not able to be read\n");
+        return NULL; // or would it be return count; or return 0; ??
+    }
+
+    return (MBR*)buffer;
 }
 
 FAT_BOOT* ReadFatBootSector(FILE* fp, long int offset)
