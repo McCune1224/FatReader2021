a4d76e7af2d37e950ac386183d140062aa256108
aamindehkordi <77307170+aamindehkordi@users.noreply.github.com>
2021-03-15 16:34:34 -0700

    Mini Code Review

--- reader.c
+++ reader.c
@@ -10,24 +10,25 @@ int ReadDiskImage(char* filename)
 MBR* ReadMsterBootRecord(FILE* fp, long int offset)
 {
     
-    int rc = fseek(fp, offset, SEEK_SET);
-    if (rc != 0) //if fseek equals 0, it is then successful. If it returns a nonzero, it has failed
+    int seek_rc = fseek(fp, offset, SEEK_SET);
+    if (seek_rc != 0) //if fseek equals 0, it is then successful. If it returns a nonzero, it has failed
     {
         printf("fseek failed, did not reach correct location\n");
-        return 0;
+        return NULL;
     }
     
     char* buffer = (char*)malloc(sizeof(MBR));
-    if (buffer == NULL) //when buffer equals, null it will return a null byte which indicates lack of memory
+    if (buffer == NULL) //when buffer equals null, it will return a null byte which indicates lack of memory
     {
         printf("Error, not enough memory\n");
         return NULL;
     }
-    int count = fread(buffer, 1, sizeof(MBR), fp); 
-    if (count < sizeof(MBR)) //if the count is less than the size of MBR, it wont be able to read the contents of the file
+    
+    int total_count = fread(buffer, 1, sizeof(MBR), fp); 
+    if (total_count < sizeof(MBR)) //if the total count is less than the size of MBR, it wont be able to read the contents of the file
     {
         printf("The contents of the file are not able to be read\n");
-        return NULL; // or would it be return count; or return 0; ??
+        return NULL; 
     }
 
     return (MBR*)buffer;
