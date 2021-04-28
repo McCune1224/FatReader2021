3fc08c4e01f27337d0cb3f34862b1a928b6119b1
Ali <aamindehkordi@gmail.com>
2021-04-28 16:29:48 -0700

    Done (Not Tested)

--- cmd.c
+++ cmd.c
@@ -50,22 +50,24 @@ void cat(char* file_path)
     char* filename = malloc(256);
     //Local Directory
     if(file_path[0] != '/')
-    {
-        strncpy(filename, dirListBuffer, sizeof(dirListBuffer));
-        strncat(filename, '/', 1);
-        strncat(filename, file_path, strlen(file_path)-1);
+    {   //The last paramater is not the size of the source.
+
+        strncpy(filename, dirListBuffer, 256);
+        strncat(filename, '/', 256-strlen(filename)-1);
+        strncat(filename, file_path, 256-strlen(filename)-1);
     }
     //Located Elsewhere
     else
     {
         filename = file_path;
     }
-    
     int size = GetFileSize(filename);
     char* buffer = GetFileData(filename);
 
+    //Cat the file
     HexDump(buffer, size);
 
+    //Maintenance
     free(filename);
     free(buffer);
     
